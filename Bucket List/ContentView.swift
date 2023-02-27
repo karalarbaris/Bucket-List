1b636ecd5d0d2197b695c5b3425195bb5a08a776
Baris Karalar <b.karalar@jemix.de>
2023-02-27 12:33:05 +0100

    Initial Commit

---
+++ Bucket List/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Bucket List
+//
+//  Created by Baris Karalar on 27.02.23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
